fe34b24c570d568a6f6e3f6ca771ef82f08343b0
joaocandidozup <joao.candido@zup.com>
2025-03-10 11:35:26 -0300

    refactor: adjusts exceptions to receive a list of validation errors

--- src/main/java/br/com/calculoImpostos/api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/com/calculoImpostos/api/exceptions/GlobalExceptionHandler.java
@@ -2,29 +2,51 @@ package br.com.calculoImpostos.api.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(TaxNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(TaxNotFoundException ex) {
-        return buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
-    }
     @ExceptionHandler(TaxAlreadyRegisteredException.class)
-    public ResponseEntity<ErrorResponse> handleResourceRegisteredException(TaxAlreadyRegisteredException ex) {
-        return buildErrorResponse(HttpStatus.CONFLICT, ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleTaxAlreadyRegisteredException(TaxAlreadyRegisteredException ex) {
+        List<String> errors = List.of(ex.getMessage());
+
+        return buildErrorResponse(HttpStatus.CONFLICT, errors);
+    }
+
+    @ExceptionHandler(TaxNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleTaxNotFoundException(TaxNotFoundException ex) {
+        List<String> errors = List.of(ex.getMessage());
+
+        return buildErrorResponse(HttpStatus.NOT_FOUND, errors);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
         ex.printStackTrace();
-        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Ocorreu um erro inesperado.");
+        List<String> errors = List.of("Ocorreu um erro inesperado.");
+
+        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, errors);
     }
 
-    private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, String message) {
-        ErrorResponse errorResponse = new ErrorResponse(status.value(), message);
-        return new ResponseEntity<>(errorResponse, status);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, errors);
     }
+
+    private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, List<String> messages) {
+        ErrorResponse errorResponse = new ErrorResponse(status.value(), messages);
+        return ResponseEntity.status(status).body(errorResponse);
+    }
+
 }
